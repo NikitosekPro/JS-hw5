a55e70bff00932450e49e5f74044ed88a47ce803
NikitosekPro <148691930+NikitosekPro@users.noreply.github.com>
2025-01-05 20:49:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,50 +2,50 @@
 // Виводьте повідомлення залежно від обраного варіанта: "Кава", "Чай" або "Сік".
 // Використайте switch.
 
-const select = "Чай" 
+// const select = "Чай" 
 
-switch (select) {
-    case "Кава":
-        console.log("Кава");
+// switch (select) {
+//     case "Кава":
+//         console.log("Кава");
         
-        break;
+//         break;
 
-    case "Чай":
-        console.log("Чай");
-        break;
+//     case "Чай":
+//         console.log("Чай");
+//         break;
 
-    case "Сік":
-        console.log("Сік");
-        break;
+//     case "Сік":
+//         console.log("Сік");
+//         break;
 
-    default:
-        console.log("Chose a drink");
-        break;
-}
+//     default:
+//         console.log("Chose a drink");
+//         break;
+// }
 
 // 2. Створіть змінну для зберігання введеного рядка,
 // який може бути днем тижня. Якщо це робочий день — виведіть повідомлення про робочий день,
 // якщо вихідний — про вихідний.
 
 
-const dayOfWeek = "friday".toLowerCase();
+// const dayOfWeek = "thursday".toLowerCase();
 
 
-const workDays = ["monday", "tuseday", "wednesday", "thursday", "friday"];
-const weekendDays = ["saturday", "sunday"];
+// const workDays = ["monday", "tuseday", "wednesday", "thursday", "friday"];
+// const weekendDays = ["saturday", "sunday"];
 
 
-if (workDays.includes(dayOfWeek)) {
+// if (workDays.includes(dayOfWeek)) {
 
-    console.log("Це робочий день.");
+//     console.log("Це робочий день.");
 
-} else if (weekendDays.includes(dayOfWeek)) {
+// } else if (weekendDays.includes(dayOfWeek)) {
 
-    console.log("Це вихідний.");
+//     console.log("Це вихідний.");
 
-} else {
-    console.log("Введено некоректний день тижня.");
-}
+// } else {
+//     console.log("Введено некоректний день тижня.");
+// }
 
 
 
@@ -53,57 +53,61 @@ if (workDays.includes(dayOfWeek)) {
 // За номером місяця визначайте пору року і виводьте відповідне повідомлення.
 
 // Створюємо змінну для зберігання номера місяця
-let monthNumber = 6;
 
 
-if (monthNumber >= 1 && monthNumber <= 2 || monthNumber === 12) {
+// let monthNumber = 13;
 
-    console.log("Це зима.");
 
-} else if (monthNumber >= 3 && monthNumber <= 5) {
 
-    console.log("Це весна.");
+// if (monthNumber >= 1 && monthNumber <= 2 || monthNumber === 12) {
 
-} else if (monthNumber >= 6 && monthNumber <= 8) {
+//     console.log("Це зима.");
 
-    console.log("Це літо.");
+// } else if (monthNumber >= 3 && monthNumber <= 5) {
 
-} else if (monthNumber >= 9 && monthNumber <= 11) {
+//     console.log("Це весна.");
 
-    console.log("Це осінь.");
+// } else if (monthNumber >= 6 && monthNumber <= 8) {
 
-} else {
-    console.log("Введено некоректний номер місяця.");
-}
+//     console.log("Це літо.");
+
+// } else if (monthNumber >= 9 && monthNumber <= 11) {
+
+//     console.log("Це осінь.");
+
+// } else {
+//     console.log("Введено некоректний номер місяця.");
+// }
+
 
 
 // 4. Створіть змінну для зберігання назви кольору.
 // Виводьте повідомлення відповідно до вибраного кольору:
 // якщо "червоний" — "стоп", "зелений" — "йти", "жовтий" — "чекати".
 
-const color = "red".toLowerCase()
+// const color = "red".toLowerCase()
 
-switch (color) {
-case "red":
-    console.log("stop");
+// switch (color) {
+// case "red":
+//     console.log("stop");
         
-        break;
+//         break;
 
-case "yellow":
-    console.log("wait");
+// case "yellow":
+//     console.log("wait");
     
-        break;
+//         break;
 
-case "green":
-    console.log("go");
+// case "green":
+//     console.log("go");
             
 
-    default:
-    console.log("Sellect any color of traffic light")
+//     default:
+//     console.log("Sellect any color of traffic light")
     
 
-        break;
-}
+//         break;
+// }
 
 
 // 5. Створіть змінні для зберігання двох чисел та оператора (як у списку select).
@@ -111,11 +115,15 @@ case "green":
 // У випадку ділення на нуль — виведіть попередження.
 
 
-const num1 = prompt("Type the first number"); 
-const num2 = prompt("Type the second number"); 
-
-
+let num1 = prompt("Type the first number");
 const operator = prompt("Chose an option: (+, -, *, /):");
+let num2 = prompt("Type the second number"); 
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+
 
 
 let result;
@@ -138,16 +146,17 @@ switch (operator) {
             result = num1 / num2;
             alert(`Result: ${num1} / ${num2} = ${result}`);
         } else {
-            alert("Error".toUpperCase());
+            alert("Nice try;)".toUpperCase());
         }
         break;
 
         case null:
             if (operator !== true) {
-                alert("Reload the page to select an option once again.");
+                
+                alert("Incorrect operator. Please, chose one of those: (+, -, *, /).");
                 
             }
 
     default:
-        console.log("Incorrect operator. Please, chose one of those ones: (+, -, *, /).");
+        console.log("Reload the page to select an option once again.");
 }
